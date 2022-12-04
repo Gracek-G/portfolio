e4ea4cb1ba881b6f8fbfe80bc7ae5723f9fe7b86
Gracek-G <gracjan.gutowski@gmail.com>
2022-12-04 18:29:12 +0100

    About section done

---
+++ src/components/about/Info.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Info = () => {
+  return (
+    <div className='about__info grid'>
+      <div className="about__box">
+      <i class='bx bxl-javascript about__icon'></i>
+        <h3 className="about__title">JavaScript ES6</h3>
+        <span className="about__subtitle"></span>
+      </div>
+
+      <div className="about__box">
+      <i class='bx bxl-react about__icon'></i>
+        <h3 className="about__title">React JS</h3>
+        <span className="about__subtitle"></span>
+      </div>
+
+      <div className="about__box">
+      <i class='bx bxl-html5 about__icon'></i>
+      <i class='bx bxl-css3 about__icon'></i>
+        <h3 className="about__title">HTML/CSS</h3>
+        <span className="about__subtitle"></span>
+      </div>
+    </div>
+  )
+}
+
+export default Info
